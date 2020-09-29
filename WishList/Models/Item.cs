3daae30460c7ba01f3bba1c6eeed90f2ac46bf5b
waninko <43968374+waninko@users.noreply.github.com>
2020-09-29 07:28:23 +0200

    forgotToSave :|

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace WishList.Models
 {
     public class Item
     {
+        public int Id;
+
+        [Required]
+        [MaxLength(50)]
+        public string Description;
+
     }
 }
