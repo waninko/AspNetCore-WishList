af1ea7b12a910a55a136f709aeba68ca86fa1449
waninko <43968374+waninko@users.noreply.github.com>
2020-10-07 10:26:46 +0200

    itemController context

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
